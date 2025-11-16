98eb8756ac7f539a856a437dc0ba031230f3a8e9
Interloper <81812373+interloper111@users.noreply.github.com>
2025-11-16 18:24:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 from standard_error import find_standard_errors
 
+# Takes data from data.txt and converts it to a matrix (assumes no header and data is separated by a comma)
 data = np.genfromtxt("data.txt",dtype='float',delimiter=',',skip_header=0)
 
+# Prints list of standard errors
 print(find_standard_errors(data))
